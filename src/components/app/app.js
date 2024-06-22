c5a9a338286514fb9e4ca3da1b66bb6cd3c66f46
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-06-22 21:15:30 +0300

    add(employees-list.js, employees-list-item.js + .css): create list-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
 
 function App() {
 	return (
@@ -12,6 +13,8 @@ function App() {
 				<SearchPanel/>
 				<AppFilter/>
 			</div>
+
+			<EmployeesList/>
 		</div>
 	);
 }
