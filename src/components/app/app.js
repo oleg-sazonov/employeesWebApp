f1ed40e9135867816a787f2707ff34fb2c9ff795
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-06-26 13:15:13 +0300

    refactor(employees-list/employees-list-item/app.js): emulate DB

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+
+	const data = [
+		{name: 'John S.', salary: 800},
+		{name: 'Alex D.', salary: 1200},
+		{name: 'Ann H.', salary: 1500}
+	];
+
 	return (
 		<div className="app">
 			<AppInfo/>
@@ -15,7 +22,7 @@ function App() {
 				<AppFilter/>
 			</div>
 
-			<EmployeesList/>
+			<EmployeesList data={data}/>
 			<EmployeesAddForm/>
 		</div>
 	);
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,13 +1,13 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = ({name, salary}) => {
 	return (
 		<li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
+            <span className="list-group-item-label">{name}</span>
             <input 
 			type="text" 
 			className="list-group-item-input" 
-			defaultValue="1000$"/>
+			defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button 
 					type="button"
